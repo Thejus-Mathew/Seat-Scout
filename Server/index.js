48d4c0eb8f31b0ab6dfba233c8e8c867e2191709
I-DUDE-I <thejusmathew8888@gmail.com>
2024-12-03 13:59:30 +0530

    updated server

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,7 @@ require("./DB/connection")
 
 const express = require('express')
 const cors = require('cors')
+const path = require('path');
 
 
 const router = require('./Router/router')
@@ -13,7 +14,7 @@ server.use(cors())
 server.use(express.json())
 server.use(router)
 // server.use('/uploads',express.static('./uploads'))
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+server.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 
 const PORT =process.env.PORT || 3000
@@ -23,6 +24,10 @@ server.listen(PORT,()=>{
     console.log(`Server started running at http://localhost:${PORT}/`);
 })
 
-server.get("/",(req,res)=>{
-    res.status(200).send("<h1>Server started running</h1>")
-})
+// server.get("/",(req,res)=>{
+//     res.status(200).send("<h1>Server started running</h1>")
+// })
+
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html')); // Send the main HTML file for all dynamic routes
+});
